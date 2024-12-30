9fa5e69729c8b50bbdd0f8eda4853ed18bcc5466
AvFl <82409897+AvFl4028@users.noreply.github.com>
2024-12-29 20:20:30 -0600

    documentation

--- src/main.py
+++ src/main.py
@@ -1,21 +1,26 @@
-import update_db
 import json
+import update_db
 
 def main():
-    json_path = "files_url.json"
+    """
+    Función principal que muestra las novelas disponibles y permite al usuario elegir una para ver sus enlaces.
+    """
+    # Cargar la información del archivo JSON
+    with open("files_url.json", "r") as file:
+        json_info = json.load(file)
 
-    with open(json_path, "r") as file:
-        json_info: dict = json.load(file)
-        file.close()
-
+    # Mostrar la lista de novelas disponibles
     for i in range(update_db.get_ln_num() + 1):
         title = json_info[str(i)]['title']
         print(f"{i}: {title}")
 
+    # Solicitar al usuario que elija una novela
     ln = int(input("Choose a novel: "))
     if ln > update_db.get_ln_num():
         print("Invalid number")
         return
+
+    # Mostrar los detalles de la novela seleccionada
     print("-----------------------------------------------------------")
     print("Title: {}".format(json_info[f"{ln}"]['title']))
     for i in range(int(list(json_info[f"{ln}"]['links']['drive'].keys())[-1]) + 1):
--- src/update_db.py
+++ src/update_db.py
@@ -1,27 +1,38 @@
+import json
 import requests
 from bs4 import BeautifulSoup
-import json
 
+# Encabezados para la solicitud HTTP
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
 }
 
+# URL del sitio web a scrapear
 url = "https://worldproject1901.wixsite.com/website/novelas-activas"
 
+# Diccionario para almacenar las URLs de los archivos
 files_url = {}
 
 def main():
+    """
+    Función principal que actualiza el contenido y guarda los datos en un archivo JSON.
+    """
     update_content()
     with open("files_url.json", "w") as file:
+        # Guarda los datos en un archivo JSON
         file.write(json.JSONEncoder().encode(files_url))
 
 def update_content():
+    """
+    Función que obtiene los enlaces y actualiza el diccionario files_url con los datos obtenidos.
+    """
     links = get_links()
     i = 0
     for link in links:
         sub_response = requests.get(link["href"], headers=headers)
         sub_soup = BeautifulSoup(sub_response.text, "html.parser")
 
+        # Actualiza el diccionario files_url con los datos obtenidos
         files_url[f"{i}"] = {}
         files_url[f"{i}"]['title'] = sub_soup.find("h1").text
         files_url[f"{i}"]['links'] = {}
@@ -47,14 +58,22 @@ def update_content():
     print(files_url)
 
 def get_ln_num() -> int:
+    """
+    Función que obtiene el número de la última novela en el archivo JSON.
+    """
     json_path = "files_url.json"
     with open(json_path, "r") as file:
         json_info: dict = json.load(file)
         file.close()
     return int(list(json_info.keys())[-1])
 
-
 def get_links():
+    """
+    Función que obtiene los enlaces de la página principal.
+    """
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser")
     return soup.find_all("a", class_="StylableButton2545352419__root")
+
+if __name__ == "__main__":
+    main()
